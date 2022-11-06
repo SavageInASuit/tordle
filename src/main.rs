64fa6bfc0bf5c72a54817bbafc299c4b2674f22a
SavageInASuit <t89savage@gmail.com>
2022-11-06 17:38:11 +0000

    Implemented real guess marking on the `/makeGuess` endpoint

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,28 @@ struct MarkedGuess {
     marks: [GuessState; 5],
 }
 
+fn get_todays_word() -> String {
+    "guess".to_owned()
+}
+
+fn in_word(c: &char, word: Vec<char>) -> bool {
+    word.contains(c)
+}
+
+fn mark_guess(guess: Guess) -> MarkedGuess {
+    let word = get_todays_word();
+    let word_chars: Vec<char> = word.chars().collect();
+    let mut marks: [GuessState; 5] = [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss];
+    for (ind, c) in guess.letters.iter().enumerate() {
+        if c == &word_chars[ind] {
+            marks[ind] = GuessState::Hit;
+        } else if in_word(c, word.chars().collect()) {
+            marks[ind] = GuessState::Letter;
+        }
+    }
+    MarkedGuess{guess, marks}
+}
+
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct UserData {
@@ -33,7 +55,7 @@ struct UserData {
 #[post("/makeGuess", data = "<guess>")]
 fn make_guess(guess: Json<Guess>) -> Json<MarkedGuess> {
     let Json(parsed_guess) = guess;
-    Json(MarkedGuess{guess: parsed_guess, marks: [GuessState::Hit, GuessState::Hit, GuessState::Hit, GuessState::Hit, GuessState::Hit]})
+    Json(mark_guess(parsed_guess))
 }
 
 #[get("/userData")]
