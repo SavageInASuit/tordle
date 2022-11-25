ca834f87f2547840109d05330e9aa48f280e7a6b
SavageInASuit <t89savage@gmail.com>
2022-11-25 22:25:48 +0000

    - Lots of updates to game process   + Different words each day   + Win message when guessing correctly   + Fixed guess marking s.t. correct letter guesses aren't duplicated - Added tests for the guessing process

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+pub mod tests;
+
+use std::collections::HashMap;
+use chrono::{Utc, Datelike};
+
+use rocket::http::{Cookie, CookieJar};
 use rocket::{get, post, launch, routes};
 use rocket::serde::{Serialize, Deserialize, json::Json};
 use rocket::fs::FileServer;
 
-#[derive(Serialize)]
+#[derive(Serialize, PartialEq, Debug)]
 #[serde(crate = "rocket::serde")]
 enum GuessState {
     Miss,
@@ -23,23 +29,72 @@ struct MarkedGuess {
     marks: [GuessState; 5],
 }
 
+
+fn get_today_formatted() -> String {
+    let now = Utc::now();
+    format!("{}-{}-{}", now.year(), now.month(), now.day())
+}
+
 fn get_todays_word() -> String {
-    "guess".to_owned()
+    let mut words: HashMap<String, String> = HashMap::new();
+    words.insert("2022-11-17".to_string(), "guess".to_string());
+    words.insert("2022-11-18".to_string(), "sting".to_string());
+    words.insert("2022-11-19".to_string(), "speak".to_string());
+    words.insert("2022-11-20".to_string(), "thine".to_string());
+    words.insert("2022-11-21".to_string(), "board".to_string());
+    words.insert("2022-11-22".to_string(), "hoard".to_string());
+    words.insert("2022-11-23".to_string(), "grove".to_string());
+    words.insert("2022-11-24".to_string(), "plonk".to_string());
+    words.insert("2022-11-25".to_string(), "wrong".to_string());
+    words.insert("2022-11-26".to_string(), "zebra".to_string());
+    words.insert("2022-11-27".to_string(), "quote".to_string());
+    words.insert("2022-11-28".to_string(), "drive".to_string());
+    words.insert("2022-11-29".to_string(), "clock".to_string());
+    words.insert("2022-11-30".to_string(), "strap".to_string());
+    let today = get_today_formatted();
+    
+    match words.get(&today) {
+        Some(word) => word.to_owned(),
+        None => "".to_owned(),
+    }
 }
 
 fn in_word(c: &char, word: Vec<char>) -> bool {
     word.contains(c)
 }
 
-fn mark_guess(guess: Guess) -> MarkedGuess {
-    let word = get_todays_word();
+fn mark_guess(guess: Guess, word: &String) -> MarkedGuess {
     let word_chars: Vec<char> = word.chars().collect();
+    let mut hits: HashMap<char, u8> = HashMap::new();
+    let mut counts: HashMap<char, u8> = HashMap::new();
     let mut marks: [GuessState; 5] = [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss];
+
+    // First pass for hits
     for (ind, c) in guess.letters.iter().enumerate() {
         if c == &word_chars[ind] {
             marks[ind] = GuessState::Hit;
-        } else if in_word(c, word.chars().collect()) {
+            let hit_count = match hits.get(c) {
+                Some(count) => count.to_owned(),
+                None => 0,
+            };
+            hits.insert(*c, hit_count + 1);
+        }
+        counts.insert(
+            *c, 
+            u8::try_from(word_chars.iter().filter(|&o| o == c).count())
+                .ok()
+                .expect("trying to convert from usize to u8")
+        );
+    }
+    // Second pass for letters
+    for (ind, c) in guess.letters.iter().enumerate() {
+        if c != &word_chars[ind] && in_word(c, word.chars().collect()) && (!hits.contains_key(c) || hits[c] != counts[c]) {
             marks[ind] = GuessState::Letter;
+            let hit_count = match hits.get(c) {
+                Some(count) => count.to_owned(),
+                None => 0,
+            };
+            hits.insert(*c, hit_count + 1);
         }
     }
     MarkedGuess{guess, marks}
@@ -55,42 +110,28 @@ struct UserData {
 #[post("/makeGuess", data = "<guess>")]
 fn make_guess(guess: Json<Guess>) -> Json<MarkedGuess> {
     let Json(parsed_guess) = guess;
-    Json(mark_guess(parsed_guess))
+    let word_of_the_day = get_todays_word();
+    Json(mark_guess(parsed_guess, &word_of_the_day))
+}
+
+fn get_user_data(cookie: &Cookie) -> UserData {
+    println!("Cookie is {:?}", cookie);
+    UserData{
+        name: "Tester".to_owned(), 
+        last: None,
+    }
 }
 
 #[get("/userData")]
-fn user_data() -> Json<UserData> {
-    Json(UserData{
-        name: "Tester".to_owned(), 
-        last: Some(
-            [
-            MarkedGuess{
-                guess: Guess{letters: ['b', 'r', 'o', 'k', 'e']},
-                marks: [GuessState::Miss, GuessState::Hit, GuessState::Letter, GuessState::Miss, GuessState::Miss],
-            },
-            MarkedGuess{
-                guess: Guess{letters: ['p', 'r', 'o', 'k', 'e']},
-                marks: [GuessState::Miss, GuessState::Hit, GuessState::Letter, GuessState::Miss, GuessState::Miss],
-            },
-            MarkedGuess{
-                guess: Guess{letters: ['p', 'z', 'z', 'z', 'z']},
-                marks: [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss],
-            },
-            MarkedGuess{
-                guess: Guess{letters: ['p', 'z', 'z', 'z', 'z']},
-                marks: [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss],
-            },
-            MarkedGuess{
-                guess: Guess{letters: ['p', 'z', 'z', 'z', 'z']},
-                marks: [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss],
-            },
-            MarkedGuess{
-                guess: Guess{letters: ['p', 'z', 'z', 'z', 'z']},
-                marks: [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss],
-            },
-            ]
-        )
-    })
+fn user_data(cookies: &CookieJar<'_>) -> Json<UserData> {
+    let user_data = match cookies.get("tordle-user") {
+        Some(cookie) => get_user_data(cookie),
+        None => UserData{
+            name: "New User".to_owned(), 
+            last: None,
+        },
+    };
+    Json(user_data)
 }
 
 #[launch]
---
+++ src/tests.rs
@@ -0,0 +1,90 @@
+#[cfg(test)]
+mod tests {
+    use crate::{Guess, mark_guess, GuessState, MarkedGuess, get_todays_word};
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    struct MarkedGuessTestCase {
+        word: String,
+        marked_guess: MarkedGuess,
+    }
+
+    fn get_marked_guesses() -> [MarkedGuessTestCase; 5] {
+        return [
+            MarkedGuessTestCase {
+                word: "guess".to_string(),
+                marked_guess: MarkedGuess {
+                    guess: Guess { letters: ['e', 'e', 'e', 'e', 'e'] },
+                    marks: [GuessState::Miss, GuessState::Miss, GuessState::Hit, GuessState::Miss, GuessState::Miss],
+                }
+            },
+            MarkedGuessTestCase {
+                word: "guess".to_string(),
+                marked_guess: MarkedGuess {
+                    guess: Guess { letters: ['w', 'r', 'o', 'n', 'g'] },
+                    marks: [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Letter],
+                }
+            },
+            MarkedGuessTestCase {
+                word: "guess".to_string(),
+                marked_guess: MarkedGuess {
+                    guess: Guess { letters: ['w', 'w', 'w', 'w', 'w'] },
+                    marks: [GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss, GuessState::Miss],
+                }
+            },
+            MarkedGuessTestCase {
+                word: "guess".to_string(),
+                marked_guess: MarkedGuess {
+                    guess: Guess { letters: ['g', 'u', 'e', 's', 's'] },
+                    marks: [GuessState::Hit, GuessState::Hit, GuessState::Hit, GuessState::Hit, GuessState::Hit],
+                }
+            },
+            MarkedGuessTestCase {
+                word: "toast".to_string(),
+                marked_guess: MarkedGuess {
+                    guess: Guess { letters: ['g', 'r', 'o', 't', 't'] },
+                    marks: [GuessState::Miss, GuessState::Miss, GuessState::Letter, GuessState::Letter, GuessState::Hit],
+                }
+            },
+        ]
+    }
+
+    #[test]
+    fn guesses_marked_correctly() {
+        let test_cases = get_marked_guesses();
+        for test_case in test_cases {
+            let marked_guess = mark_guess(test_case.marked_guess.guess, &test_case.word);
+            let marks = marked_guess.marks;
+
+            assert_eq!(
+                marks, 
+                test_case.marked_guess.marks
+            );
+        }
+    }
+
+    #[test]
+    fn guess_good_request_okay_response() {
+        let client = Client::tracked(crate::rocket()).expect("valid `rocket`");
+
+        let response = client.post("/makeGuess").body("{\"letters\": [\"a\",\"b\",\"c\",\"d\",\"e\"]}").dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn guess_bad_request_bad_request_response() {
+        let client = Client::tracked(crate::rocket()).expect("valid `rocket`");
+
+        let response = client.post("/makeGuess").body("{\"letters\": [\"a\"]}").dispatch();
+
+        assert_eq!(response.status(), Status::UnprocessableEntity);
+    }
+
+    #[test]
+    fn todays_word_valid() {
+        let word_of_the_day = get_todays_word();
+
+        assert!(word_of_the_day.len() == 5);
+    }
+}
